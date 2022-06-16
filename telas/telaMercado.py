362e1d7ac59d601dad13ff588aaa72ac9b0eae17
Mathias Fernandes <mathias.silva.fernandes@gmail.com>
2022-06-15 21:08:45 -0300

    Implementa funcao para mostrar pegar e mostrar dados do mercado

--- telas/telaMercado.py
+++ telas/telaMercado.py
@@ -1,1 +1,22 @@
-1
+class TelaMercado:
+    def mostra_mensagem(self, mensagem):
+        print(mensagem)
+
+    def pega_dados_mercado(self):
+        while True:
+            dados = {}
+            print("---- DADOS DO MERCADO ----")
+            dados["nome"] = input("Nome do mercado: ")
+            dados["cep"] = input("CEP do mercado: ")
+            dados["numero"] = input("Numero do endereço do mercado: ")
+            dados["cnpj"] = input("CNPJ do mercado: ")
+
+            if not self.verifica_integridade_dados():
+                self.mostra_mensagem("Dados inválidos, tente novamente!")
+                continue
+            return dados
+
+    def mostra_dados_mercado(self, dados):
+        print("Nome do mercado: ", dados["nome"])
+        print("CEP do mercado: ", dados["cep"])
+        print("CNPJ do mercado: ", dados["cnpj"])
