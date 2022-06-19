8e5213b7ef8909d93b8ef0a318cc9465988be935
Mathias Fernandes <mathias.silva.fernandes@gmail.com>
2022-06-19 04:55:07 -0300

    Implementa categoria

--- controladores/controladorCategoria.py
+++ controladores/controladorCategoria.py
@@ -1,1 +1,29 @@
-1
+from entidades.categoria import Categoria
+from telas.telaCategoria import TelaCategoria
+
+CATEGORIAS = [
+    "carne",
+    "higiene",
+    "limpeza",
+    "bebida",
+    "hortfruti",
+]
+
+
+class ControladorCategoria:
+    def __init__(self, controlador_sistema):
+        self.__controlador_sistema = controlador_sistema
+        self.__categorias = [Categoria(c) for c in CATEGORIAS]
+        self.__tela_categoria = TelaCategoria()
+
+    @property
+    def controlador_sistema(self):
+        return self.__controlador_sistema
+
+    @property
+    def categorias(self):
+        return self.__categorias
+
+    @property
+    def tela_categoria(self):
+        return self.__tela_categoria
--- telas/telaCategoria.py
+++ telas/telaCategoria.py
@@ -1,1 +1,4 @@
-1
+class TelaCategoria:
+    
+    def mostra_categoria(self, categoria):
+        print(f'CATEGORIA: {categoria.nome}')
