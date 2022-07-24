dc8295f59f5d6c92cbd97ad5d55e49f8e02bc26c
davi <davi@pop-os.localdomain>
2022-07-24 19:28:05 -0300

    Att tela_categoria

--- telas/tela_categoria.py
+++ telas/tela_categoria.py
@@ -1,5 +1,5 @@
+import PySimpleGUI as sg
 from telas.tela_abstrata import TelaAbstrata
-
 
 class TelaCategoria(TelaAbstrata):
     CATEGORIAS = [
@@ -11,18 +11,33 @@ class TelaCategoria(TelaAbstrata):
     ]
 
     def mostra_categoria(self, categoria):
-        print(f"CATEGORIA: {categoria.nome}")
+        sg.theme('SandyBeach') 
+        sg.Print({categoria.nome}, do_not_reroute_stdout=False)
 
     def pega_nome_categoria(self):
-        nome = input("Nome da categoria: ")
+        sg.theme('SandyBeach') 
+        layout = [
+            [sg.Text('Digite o nome da categoria')],
+            [sg.Text('Nome da categoria', size =(15, 1)), sg.InputText()],
+            [sg.Enviar(), sg.Cancelar()]
+        ]
+
+        window = sg.Window('Categoria', layout)
+        nome = window.read()
+        window.close()
         return nome
 
     def mostra_categorias(self):
-        print("[0] - CARNE")
-        print("[1] - HIGIENE")
-        print("[2] - LIMPEZA")
-        print("[3] - BEBIDA")
-        print("[4] - HORTFRUTI")
-        return self.le_numero_inteiro(
-            "Selecione uma das categorias acima: ", [0, 1, 2, 3, 4]
-        )
+        sg.theme('SandyBeach')
+        layout = [
+            [sg.Radio('Carne', '0', default=True)],
+            [sg.Radio('Higiene', '1'),]
+            [sg.Radio('Limpeza', '2'),]
+            [sg.Radio('Bebida', '3')]
+            [sg.Radio('Hortfruti' '4')],
+            [sg.Button('Enviar'), sg.Cancelar()]
+        ]
+
+        window = sg.Window('Categoria', layout)
+        window.close()
+        return window.read()
